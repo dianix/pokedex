d454f02ef5480454385a09ed168ddf28d84577a4
dianix <dianavazquez.dcg@gmail.com>
2017-06-12 13:32:36 -0500

    modal muestra datos de pokemon seleccionado

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var cargarPagina = function() {
     $('.modal').modal();
-    $("body").on("click",".datos", mostrarDatos);
+    $("body").on("click",".datos", obtenerDatos);
     $.getJSON("https://pokeapi.co/api/v2/pokemon/", function (response) {
         //console.log(response);
         var pokemons = response.results;
@@ -14,7 +14,6 @@ function crearPokemons(pokemons) {
     $(pokemons).each(function (i, pokemon) {
         var cajaPokemon = $("<div class='card col s6 m3 hoverable'/>");
         var pokeFoto = $("<img />").attr("src", pokeimagenes[i].imagen);
-        //console.log(pokeFoto)
         var nombre = $("<a class='datos center-text'/>").text(pokemon.name).attr("data-info", pokemon.url).attr('href', "#modal1");
         cajaPokemon.append(pokeFoto).append(nombre);
         fila.append(cajaPokemon);
@@ -34,42 +33,51 @@ var plantillaModal = '<div class="row">'+
                                  '<h6 class="col s6">Color: __color__</h6>'+
                                  '<h6 class="col s6">Shape: __forma__</h6>' +                               
                                  '<h6 class="col s6">Habitat: __habitat__</h6>'+
-                                 '<h6 class="col s6">Genero: __genero__</h6>'+
+                                 '<h6 class="col s6">Genero: __genera__</h6>'+
                             '</div>'+
                          '</div>'+
                      '</div>';
 
-var mostrarDatos = function(){
+var obtenerDatos = function(){
     var info = $(this).data("info");
-    //console.log(this)
+    var foto = $(this).prev().attr("src");
+    var nombre = $(this).text();
+    console.log(nombre)
     var $modal = $("#modal1")
     $.getJSON(info,function(response){
-        console.log(response)
+        //console.log(response)
         var especie = response.species.url;
-        datosPokemon(especie);
-        //console.log(especie)    
-        
+        $.getJSON(especie,function(response){
+            var habitat = response.habitat.name;
+            var color = response.color.name;
+            var shape = response.shape.name;
+            var genera = response.genera[0].genus;
+            
+            mostrarDatos({
+                foto:foto,
+                nombre:nombre,
+                habitat:habitat,
+                color:color,
+                shape:shape,
+                genera:genera
+            });
+        })     
     });    
 };
 
-var datosPokemon = function(especie){
-    console.log(especie)
-    $.getJSON(especie,function(response){
-            console.log(response);
-            console.log(response.habitat.name);
-            console.log(response.color.name);
-            console.log(response.shape.name);
-            console.log(response.genera[0].genus);
-        })
+var mostrarDatos = function(datos){
+    console.log(datos)
+    var contenedorModal = $(".modal-content");
+    var plantillaFinal = "";
+    plantillaFinal += plantillaModal.replace("__src__",datos.foto)
+                .replace("__pokemon__",datos.nombre)
+                .replace("__nombrePokemon__",datos.nombre)
+                .replace("__color__",datos.color)
+                .replace("__forma__",datos.shape)
+                .replace("__habitat__",datos.habitat)
+                .replace("__genera__",datos.genera);
+    contenedorModal.html(plantillaFinal);
 };
-
-/*var obtenerDatos = function(){
-    $.getJSON(dato,function(response){
-        var contenido = response
-    })
-}*/
-
-
 
 var pokeimagenes = [
     {
@@ -133,6 +141,5 @@ var pokeimagenes = [
         "imagen": "assets/imgs/20-Raticate.png"
     }
 ];
-
 
 $(document).ready(cargarPagina);
